1dda71a6e5c8b9fb9ff717e556e06cd88c3e75bc
Demolka183 <166244786+Demolka183@users.noreply.github.com>
2024-04-07 12:12:23 +0200

    Create main.java

---
+++ main.java
@@ -0,0 +1,13 @@
+import geometry.Point;
+import geometry.ColoredCircle;
+public class main {
+    public static void main(String[] args) {
+        Point center = new Point(0, 0);
+        ColoredCircle coloredCircle = new ColoredCircle(center, 5, "Red");
+        System.out.println("Colored Center: (" + coloredCircle.getCenter().getX() + ", " + coloredCircle.getCenter().getY() + ")");
+        System.out.println("Colored Radius: " + coloredCircle.getRadius());
+        System.out.println("Colored Perimeter: " + coloredCircle.calculatePerimeter());
+        System.out.println("Colored Area: " + coloredCircle.getArea());
+        System.out.println("Colored Color: " + coloredCircle.getColor());
+    }
+}
