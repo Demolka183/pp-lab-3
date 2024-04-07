0e9976e07c749f3a46b052f061f07324aacc1012
Demolka183 <166244786+Demolka183@users.noreply.github.com>
2024-04-07 12:42:09 +0200

    Update Point.java

--- geometry/Point.java
+++ geometry/Point.java
@@ -1,23 +1,22 @@
 package geometry;
+
 public class Point {
-	private double x;
-	private double y;
-	public double getX() {
-		return x;
-	}
-	public double getY() {
-		return y;
-	}
-	public void setX(int z) {
-		x = z;
-	}
-	public void setY(int z) {
-		y = z;
-	}
-	public Point(){
-		System.out.println("Jestem konstruktorem");
-		x = 0;
-		y = 0;
-	}
-	
+    private double x;
+    private double y;
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    public double getX() {
+        return x;
+    }
+    public double getY() {
+        return y;
+    }
+    public void setX(double x) {
+        this.x = x;
+    }
+    public void setY(double y) {
+        this.y = y;
+    }
 }
