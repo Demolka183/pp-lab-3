8dfebb522a75b5e7cb3ce403a49dba62d5eea814
Demolka183 <166244786+Demolka183@users.noreply.github.com>
2024-04-07 12:00:09 +0200

    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+import geometry.Point;
+import geometry.Circle;
+public class main {
+    public static void main(String[] args) {
+        Point center = new Point(0, 0);
+        Circle circle = new Circle(center, 8);
+        System.out.println("Center: (" + circle.getCenter().getX() + ", " + circle.getCenter().getY() + ")");
+        System.out.println("Radius: " + circle.getRadius());
+        System.out.println("Perimeter: " + circle.calculatePerimeter());
+        System.out.println("Area: " + circle.getArea());
+    }
+}
