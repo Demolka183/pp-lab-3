11195abf084d1f0e341ae7280ffa4dd3677ac178
Demolka183 <166244786+Demolka183@users.noreply.github.com>
2024-04-07 12:34:03 +0200

    Create Circle.java

---
+++ geometry/Circle.java
@@ -0,0 +1,25 @@
+package geometry;
+
+public class Circle {
+    private geometry.Point center;
+    private double radius;
+    public Circle(geometry.Point center, double radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+    public geometry.Point getCenter() {
+        return center;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public double calculatePerimeter() {
+        return 2 * Math.PI * radius;
+    }
+    public double getArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+    public double calculateArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+}
